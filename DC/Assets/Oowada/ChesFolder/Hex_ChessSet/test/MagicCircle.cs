2ab1f7e47bde4539c7d6b86b0aabd7c2828fefea
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2022-05-09 18:06:09 +0900

    回転させました

---
+++ DC/Assets/Oowada/ChesFolder/Hex_ChessSet/test/MagicCircle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircle : MonoBehaviour
+{
+    public float CircleR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // ローカル座標基準で、現在の回転量へ加算する
+        myTransform.Rotate(0, 0, CircleR * Time.deltaTime);
+    }
+}
