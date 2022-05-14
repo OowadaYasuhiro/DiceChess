cf90675073c4c1ebdf4669209122342d6e1c6c2c
2PC-38\student <20cge0021wt@edu.sendai-com.ac.jp>
2022-05-14 13:55:54 +0900

    se

---
+++ DC/DC/Assets/title/code/Selecttable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Selecttable : MonoBehaviour
+{
+    Button チュートリアル;
+    Button START;
+    Button EXIT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        チュートリアル　= GameObject.Find("チュートリアルUI/チュートリアル").GetComponent<Button>();
+        
+        START = GameObject.Find("STARTUI/START").GetComponent<Button>();
+
+        EXIT = GameObject.Find("EXITUI/EXIT").GetComponent<Button>();
+    }
+
+}
+
