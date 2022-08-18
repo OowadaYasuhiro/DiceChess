400918060138ecaec44a9fee13c438f56f17c159
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2022-08-18 21:05:35 +0900

    CSVファイル実装

--- DC/Assets/Ozaki/Script/SpecialMoveController.cs
+++ DC/Assets/Ozaki/Script/SpecialMoveController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using System.IO;
+
 
 public class SpecialMoveController : MonoBehaviour
 {
     //必殺技ゲージ
-    public int damage = 3;
-    public int minSp = 15;
+    private int damage = 3;
+    private int minSp = 15;
     int hp;
+
+    //csv
+    TextAsset csvFile; 
+    List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+    public Vector3 position;
+    public Quaternion rotation;
 
     //相手のステータスを読み取るためのヤツ    
     CharacterStatus player1Chara; //プレイヤー1キャラのスクリプトを読み込む(読み込み対象変わるかも)
@@ -42,6 +50,22 @@ public class SpecialMoveController : MonoBehaviour
         effCon = GameObject.Find("SceneDirector").GetComponent<EffectController>();
         fadeAnim = GameObject.Find("AttackBackPanel").GetComponent<Animator>();
         txtResultInfo = GameObject.Find("TextResultInfo");
+
+        //csv
+        csvFile = Resources.Load("CSV/ItemSpecial") as TextAsset; // Resouces下のCSV読み込み
+        StringReader reader = new StringReader(csvFile.text);
+
+        // , で分割しつつ一行ずつ読み込み
+        // リストに追加していく
+        while (reader.Peek() != -1) // reader.Peaekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+        damage = int.Parse(csvDatas[0][1]);
+        Debug.Log("必殺1 ダメージ" + csvDatas[0][1]);
+        minSp = int.Parse(csvDatas[0][2]);
+        Debug.Log("必殺4 SP吸収" + csvDatas[0][2]);
     }
 
     // Update is called once per frame
@@ -91,21 +115,22 @@ public class SpecialMoveController : MonoBehaviour
                 unitCon.SetHP(hp);
                 Debug.Log(unit.name);
 
-                if (unitCon.GetTYPE() == 1 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakePawn++; }
-                if (unitCon.GetTYPE() == 1 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakePawn++; }
-                if (unitCon.GetTYPE() == 2 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeRook++; }
-                if (unitCon.GetTYPE() == 2 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeRook++; }
-                if (unitCon.GetTYPE() == 3 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeKnight++; }
-                if (unitCon.GetTYPE() == 3 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeKnight++; }
-                if (unitCon.GetTYPE() == 4 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeBishop++; }
-                if (unitCon.GetTYPE() == 4 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeBishop++; }
-                if (unitCon.GetTYPE() == 5 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeQueen++; }
-                if (unitCon.GetTYPE() == 5 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeQueen++; }
-                if (unitCon.GetTYPE() == 6 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeKing++; }
-                if (unitCon.GetTYPE() == 6 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeKing++; }
-                if (unitCon.GetTYPE() == 6)
+                int a = unitCon.GetTYPE();
+                if (a == 1 && hp <= 0) { DontDestroySingleObject.p1TakePawn++; }
+                if (a == 1 && hp <= 0) { DontDestroySingleObject.p2TakePawn++; }
+                if (a == 2 && hp <= 0) { DontDestroySingleObject.p1TakeRook++; }
+                if (a == 2 && hp <= 0) { DontDestroySingleObject.p2TakeRook++; }
+                if (a == 3 && hp <= 0) { DontDestroySingleObject.p1TakeKnight++; }
+                if (a == 3 && hp <= 0) { DontDestroySingleObject.p2TakeKnight++; }
+                if (a == 4 && hp <= 0) { DontDestroySingleObject.p1TakeBishop++; }
+                if (a == 4 && hp <= 0) { DontDestroySingleObject.p2TakeBishop++; }
+                if (a == 5 && hp <= 0) { DontDestroySingleObject.p1TakeQueen++; }
+                if (a == 5 && hp <= 0) { DontDestroySingleObject.p2TakeQueen++; }
+                if (a == 6 && hp <= 0) { DontDestroySingleObject.p1TakeKing++; }
+                if (a == 6 && hp <= 0) { DontDestroySingleObject.p2TakeKing++; }
+                if (a == 6)
                 {
-                    if(unitCon.GetHP() <= 0)
+                    if(hp <= 0)
                     {
                         effCon.enemyPositionEff(4, enemyVec);
                         Text info = txtResultInfo.GetComponent<Text>();
@@ -157,22 +182,23 @@ public class SpecialMoveController : MonoBehaviour
                 unitCon.SetHP(hp);
                 Debug.Log(unit.name);
 
-
-                if (unitCon.GetTYPE() == 1 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakePawn++; DontDestroySingleObject.p1Point += unitCon.GetPOINT();}
-                if (unitCon.GetTYPE() == 1 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakePawn++; DontDestroySingleObject.p2Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 2 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeRook++; DontDestroySingleObject.p1Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 2 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeRook++; DontDestroySingleObject.p2Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 3 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeKnight++; DontDestroySingleObject.p1Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 3 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeKnight++; DontDestroySingleObject.p2Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 4 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeBishop++; DontDestroySingleObject.p1Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 4 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeBishop++; DontDestroySingleObject.p2Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 5 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeQueen++; DontDestroySingleObject.p1Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 5 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeQueen++; DontDestroySingleObject.p2Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 6 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p1TakeKing++; DontDestroySingleObject.p1Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 6 && unitCon.GetHP() <= 0) { DontDestroySingleObject.p2TakeKing++; DontDestroySingleObject.p2Point += unitCon.GetPOINT(); }
-                if (unitCon.GetTYPE() == 6)
+                int a = unitCon.GetTYPE();
+                int b = unitCon.GetPOINT();
+                if (a == 1 && hp <= 0) { DontDestroySingleObject.p1TakePawn++; DontDestroySingleObject.p1Point += b;}
+                if (a == 1 && hp <= 0) { DontDestroySingleObject.p2TakePawn++; DontDestroySingleObject.p2Point += b; }
+                if (a == 2 && hp <= 0) { DontDestroySingleObject.p1TakeRook++; DontDestroySingleObject.p1Point += b; }
+                if (a == 2 && hp <= 0) { DontDestroySingleObject.p2TakeRook++; DontDestroySingleObject.p2Point += b; }
+                if (a == 3 && hp <= 0) { DontDestroySingleObject.p1TakeKnight++; DontDestroySingleObject.p1Point += b; }
+                if (a == 3 && hp <= 0) { DontDestroySingleObject.p2TakeKnight++; DontDestroySingleObject.p2Point += b; }
+                if (a == 4 && hp <= 0) { DontDestroySingleObject.p1TakeBishop++; DontDestroySingleObject.p1Point += b; }
+                if (a == 4 && hp <= 0) { DontDestroySingleObject.p2TakeBishop++; DontDestroySingleObject.p2Point += b; }
+                if (a == 5 && hp <= 0) { DontDestroySingleObject.p1TakeQueen++; DontDestroySingleObject.p1Point += b; }
+                if (a == 5 && hp <= 0) { DontDestroySingleObject.p2TakeQueen++; DontDestroySingleObject.p2Point += b; }
+                if (a == 6 && hp <= 0) { DontDestroySingleObject.p1TakeKing++; DontDestroySingleObject.p1Point += b; }
+                if (a == 6 && hp <= 0) { DontDestroySingleObject.p2TakeKing++; DontDestroySingleObject.p2Point += b; }
+                if (a == 6)
                 {
-                    if (unitCon.GetHP() <= 0)
+                    if (hp <= 0)
                     {
                         effCon.enemyPositionEff(4, enemyVec);
                         Text info = txtResultInfo.GetComponent<Text>();
